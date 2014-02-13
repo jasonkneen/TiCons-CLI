3fbed85c7953085725da1e6157179a0412b9367b
Fokke Zandbergen <mail@fokkezb.nl>
2014-02-13 16:06:06 +0100

    really fixing tests

--- test/icons.js
+++ test/icons.js
@@ -28,31 +28,31 @@ describe('icons', function() {
         }
 
         output.should.be.eql({
-          "ios-appicon-60": tmpDir + "tmp/app/assets/iphone/appicon-60.png",
-          "ios-appicon@2x": tmpDir + "tmp/app/assets/iphone/appicon@2x.png",
-          "ios-appicon": tmpDir + "tmp/app/assets/iphone/appicon.png",
-          "ios-appicon-60@2x": tmpDir + "tmp/app/assets/iphone/appicon-60@2x.png",
-          "ios-appicon-72": tmpDir + "tmp/app/assets/iphone/appicon-72.png",
-          "ios-appicon-Small": tmpDir + "tmp/app/assets/iphone/appicon-Small.png",
-          "ios-appicon-Small@2x": tmpDir + "tmp/app/assets/iphone/appicon-Small@2x.png",
-          "ios-appicon-72@2x": tmpDir + "tmp/app/assets/iphone/appicon-72@2x.png",
-          "ios-appicon-76": tmpDir + "tmp/app/assets/iphone/appicon-76.png",
-          "ios-appicon-Small-40": tmpDir + "tmp/app/assets/iphone/appicon-Small-40.png",
-          "ios-appicon-76@2x": tmpDir + "tmp/app/assets/iphone/appicon-76@2x.png",
-          "ios-iTunesArtwork": tmpDir + "tmp/app/assets/iphone/iTunesArtwork",
+          "ios-appicon-60": tmpDir + "/app/assets/iphone/appicon-60.png",
+          "ios-appicon@2x": tmpDir + "/app/assets/iphone/appicon@2x.png",
+          "ios-appicon": tmpDir + "/app/assets/iphone/appicon.png",
+          "ios-appicon-60@2x": tmpDir + "/app/assets/iphone/appicon-60@2x.png",
+          "ios-appicon-72": tmpDir + "/app/assets/iphone/appicon-72.png",
+          "ios-appicon-Small": tmpDir + "/app/assets/iphone/appicon-Small.png",
+          "ios-appicon-Small@2x": tmpDir + "/app/assets/iphone/appicon-Small@2x.png",
+          "ios-appicon-72@2x": tmpDir + "/app/assets/iphone/appicon-72@2x.png",
+          "ios-appicon-76": tmpDir + "/app/assets/iphone/appicon-76.png",
+          "ios-appicon-Small-40": tmpDir + "/app/assets/iphone/appicon-Small-40.png",
+          "ios-appicon-76@2x": tmpDir + "/app/assets/iphone/appicon-76@2x.png",
+          "ios-iTunesArtwork": tmpDir + "/app/assets/iphone/iTunesArtwork",
           "android-appicon-ldpi": tmpDir + "tmp/platform/android/res/drawable-ldpi/appicon.png",
-          "ios-appicon-Small-40@2x": tmpDir + "tmp/app/assets/iphone/appicon-Small-40@2x.png",
-          "ios-appicon-Small-50": tmpDir + "tmp/app/assets/iphone/appicon-Small-50.png",
-          "ios-appicon-Small-50@2x": tmpDir + "tmp/app/assets/iphone/appicon-Small-50@2x.png",
+          "ios-appicon-Small-40@2x": tmpDir + "/app/assets/iphone/appicon-Small-40@2x.png",
+          "ios-appicon-Small-50": tmpDir + "/app/assets/iphone/appicon-Small-50.png",
+          "ios-appicon-Small-50@2x": tmpDir + "/app/assets/iphone/appicon-Small-50@2x.png",
           "android-appicon-mdpi": tmpDir + "tmp/platform/android/res/drawable-mdpi/appicon.png",
-          "android-appicon": tmpDir + "tmp/app/assets/android/appicon.png",
+          "android-appicon": tmpDir + "/app/assets/android/appicon.png",
           "android-appicon-hdpi": tmpDir + "tmp/platform/android/res/drawable-hdpi/appicon.png",
           "android-appicon-xhdpi": tmpDir + "tmp/platform/android/res/drawable-xhdpi/appicon.png",
-          "ios-iTunesArtwork@2x": tmpDir + "tmp/app/assets/iphone/iTunesArtwork@2x",
-          "tizen-appicon": tmpDir + "tmp/app/assets/tizen/appicon.png",
+          "ios-iTunesArtwork@2x": tmpDir + "/app/assets/iphone/iTunesArtwork@2x",
+          "tizen-appicon": tmpDir + "/app/assets/tizen/appicon.png",
           "android-appicon-xxhdpi": tmpDir + "tmp/platform/android/res/drawable-xxhdpi/appicon.png",
-          "blackberry-appicon": tmpDir + "tmp/app/assets/blackberry/appicon.png",
-          "mobileweb-appicon": tmpDir + "tmp/app/assets/mobileweb/appicon.png"
+          "blackberry-appicon": tmpDir + "/app/assets/blackberry/appicon.png",
+          "mobileweb-appicon": tmpDir + "/app/assets/mobileweb/appicon.png"
         });
 
         should(_.every(output, function(output, name) {
@@ -66,7 +66,7 @@ describe('icons', function() {
     });
 
     after(function() {
-      //fs.deleteDirSync(tmpDir);
+      fs.deleteDirSync(tmpDir);
     });
 
   });
--- test/splashes.js
+++ test/splashes.js
@@ -28,28 +28,28 @@ describe('splashes', function() {
         }
 
         output.should.be.eql({
-          "ios-Default": tmpDir + "tmp/app/assets/iphone/Default.png",
-          "ios-Default@2x": tmpDir + "tmp/app/assets/iphone/Default@2x.png",
-          "ios-Default-568h@2x": tmpDir + "tmp/app/assets/iphone/Default-568h@2x.png",
-          "ios-Default-Portrait": tmpDir + "tmp/app/assets/iphone/Default-Portrait.png",
-          "ios-Default-Portrait@2x": tmpDir + "tmp/app/assets/iphone/Default-Portrait@2x.png",
-          "ios-Default-Landscape": tmpDir + "tmp/app/assets/iphone/Default-Landscape.png",
-          "ios-Default-Landscape@2x": tmpDir + "tmp/app/assets/iphone/Default-Landscape@2x.png",
+          "ios-Default": tmpDir + "/app/assets/iphone/Default.png",
+          "ios-Default@2x": tmpDir + "/app/assets/iphone/Default@2x.png",
+          "ios-Default-568h@2x": tmpDir + "/app/assets/iphone/Default-568h@2x.png",
+          "ios-Default-Portrait": tmpDir + "/app/assets/iphone/Default-Portrait.png",
+          "ios-Default-Portrait@2x": tmpDir + "/app/assets/iphone/Default-Portrait@2x.png",
+          "ios-Default-Landscape": tmpDir + "/app/assets/iphone/Default-Landscape.png",
+          "ios-Default-Landscape@2x": tmpDir + "/app/assets/iphone/Default-Landscape@2x.png",
           "android-default-notlong-land-ldpi": tmpDir + "tmp/platform/android/res/drawable-land-ldpi/splash.9.png",
-          "mobileweb-Default-jpg": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default.jpg",
+          "mobileweb-Default-jpg": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default.jpg",
           "android-default-notlong-land-mdpi": tmpDir + "tmp/platform/android/res/drawable-land-mdpi/splash.9.png",
           "android-default-notlong-port-ldpi": tmpDir + "tmp/platform/android/res/drawable-port-ldpi/splash.9.png",
-          "mobileweb-Default-Portrait-jpg": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default-Portrait.jpg",
-          "mobileweb-Default-Landscape-jpg": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default-Landscape.jpg",
+          "mobileweb-Default-Portrait-jpg": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default-Portrait.jpg",
+          "mobileweb-Default-Landscape-jpg": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default-Landscape.jpg",
           "android-default-notlong-land-hdpi": tmpDir + "tmp/platform/android/res/drawable-land-hdpi/splash.9.png",
           "android-default-notlong-port-mdpi": tmpDir + "tmp/platform/android/res/drawable-port-mdpi/splash.9.png",
-          "mobileweb-Default-png": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default.png",
+          "mobileweb-Default-png": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default.png",
           "android-default-notlong-land-xhdpi": tmpDir + "tmp/platform/android/res/drawable-land-xhdpi/splash.9.png",
           "android-default-notlong-port-hdpi": tmpDir + "tmp/platform/android/res/drawable-port-hdpi/splash.9.png",
           "android-default-notlong-port-xhdpi": tmpDir + "tmp/platform/android/res/drawable-port-xhdpi/splash.9.png",
-          "blackberry-splash-600x1024": tmpDir + "tmp/app/assets/blackberry/splash-600x1024.png",
-          "mobileweb-Default-Portrait-png": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default-Portrait.png",
-          "mobileweb-Default-Landscape-png": tmpDir + "tmp/app/assets/mobileweb/apple_startup_images/Default-Landscape.png"
+          "blackberry-splash-600x1024": tmpDir + "/app/assets/blackberry/splash-600x1024.png",
+          "mobileweb-Default-Portrait-png": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default-Portrait.png",
+          "mobileweb-Default-Landscape-png": tmpDir + "/app/assets/mobileweb/apple_startup_images/Default-Landscape.png"
         });
 
         should(_.every(output, function(output, name) {
@@ -63,7 +63,7 @@ describe('splashes', function() {
     });
 
     after(function() {
-      //fs.deleteDirSync(tmpDir);
+      fs.deleteDirSync(tmpDir);
     });
 
   });

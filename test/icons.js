6f09c46b849c78996c609c12378fbd3f440d5b6b
Fokke Zandbergen <mail@fokkezb.nl>
2015-09-04 15:46:18 +0200

    Update apple-watch support, add sdk-version option

--- test/icons.js
+++ test/icons.js
@@ -11,7 +11,7 @@ describe('icons', function() {
   describe('alloy', function() {
     this.timeout(10000);
 
-    before(function() {
+    beforeEach(function() {
       fs.createDir(tmpDir, 0755);
     });
 
@@ -22,6 +22,62 @@ describe('icons', function() {
         minDpi: 120,
         maxDpi: 640,
         outputDir: tmpDir,
+        radius: 18,
+        alloy: true
+      }, function(err, output) {
+
+        if (err) {
+          return done(new Error(err));
+        }
+
+        _.sortBy(output, _.identity).should.be.eql(_.sortBy({
+          "ios-iTunesConnect": path.join(tmpDir,"/iTunesConnect.png"),
+          "ios-appicon": path.join(tmpDir,"/app/assets/iphone/appicon.png"),
+          "ios-iTunesArtwork": path.join(tmpDir,"/app/assets/iphone/iTunesArtwork"),
+          "android-MarketplaceArtwork": path.join(tmpDir,"/MarketplaceArtwork.png"),
+          "android-appicon-ldpi": path.join(tmpDir,"/platform/android/res/drawable-ldpi/appicon.png"),
+          "android-appicon-mdpi": path.join(tmpDir,"/platform/android/res/drawable-mdpi/appicon.png"),
+          "android-appicon": path.join(tmpDir,"/app/assets/android/appicon.png"),
+          "android-appicon-hdpi": path.join(tmpDir,"/platform/android/res/drawable-hdpi/appicon.png"),
+          "android-appicon-xxxhdpi": path.join(tmpDir,"/platform/android/res/drawable-xxxhdpi/appicon.png"),
+          "ios-iTunesArtwork@2x": path.join(tmpDir,"/app/assets/iphone/iTunesArtwork@2x"),
+          "tizen-appicon": path.join(tmpDir,"/app/assets/tizen/appicon.png"),
+          "android-appicon-xxhdpi": path.join(tmpDir,"/platform/android/res/drawable-xxhdpi/appicon.png"),
+          "android-appicon-xhdpi": path.join(tmpDir,"/platform/android/res/drawable-xhdpi/appicon.png"),
+          "blackberry-appicon": path.join(tmpDir,"/app/assets/blackberry/appicon.png"),
+          "mobileweb-appicon": path.join(tmpDir,"/app/assets/mobileweb/appicon.png"),
+          "apple-watch-icon-24@2x.png": path.join(tmpDir,"/apple-watch/icon-24@2x.png"),
+          "apple-watch-icon-27.5@2x.png": path.join(tmpDir,"/apple-watch/icon-27.5@2x.png"),
+          "apple-watch-icon-29@2x.png": path.join(tmpDir,"/apple-watch/icon-29@2x.png"),
+          "apple-watch-icon-29@3x.png": path.join(tmpDir,"/apple-watch/icon-29@3x.png"),
+          "apple-watch-icon-40@2x.png": path.join(tmpDir,"/apple-watch/icon-40@2x.png"),
+          "apple-watch-icon-44@2x.png": path.join(tmpDir,"/apple-watch/icon-44@2x.png"),
+          "apple-watch-icon-86@2x.png": path.join(tmpDir,"/apple-watch/icon-86@2x.png"),
+          "apple-watch-icon-98@2x.png": path.join(tmpDir,"/apple-watch/icon-98@2x.png"),
+          "windows-Logo":path.join(tmpDir,"/app/assets/windows/Logo.png"),
+          "windows-SmallLogo":path.join(tmpDir,"/app/assets/windows/SmallLogo.png"),
+          "windows-Square44x44Logo.scale-100":path.join(tmpDir,"/app/assets/windows/Square44x44Logo.scale-100.png"),
+          "windows-StoreLogo":path.join(tmpDir,"/app/assets/windows/StoreLogo.png")
+        }, _.identity));
+
+        should(_.every(output, function(output, name) {
+          return fs.existsSync(output);
+        })).be.true;
+
+        done();
+
+      });
+
+    });
+
+    it('generates expected files for 4.0.0', function(done) {
+
+      ticons.icons({
+        input: path.join(__dirname, 'icon.png'),
+        minDpi: 120,
+        maxDpi: 640,
+        outputDir: tmpDir,
+        sdkVersion: '4.0.0',
         radius: 18,
         alloy: true
       }, function(err, output) {
@@ -61,14 +117,14 @@ describe('icons', function() {
           "android-appicon-xhdpi": path.join(tmpDir,"/platform/android/res/drawable-xhdpi/appicon.png"),
           "blackberry-appicon": path.join(tmpDir,"/app/assets/blackberry/appicon.png"),
           "mobileweb-appicon": path.join(tmpDir,"/app/assets/mobileweb/appicon.png"),
-          "apple-watch-40x40@2x": path.join(tmpDir,"/apple-watch/40x40@2x.png"),
-          "apple-watch-44x44@2x": path.join(tmpDir,"/apple-watch/44x44@2x.png"),
-          "apple-watch-86x86@2x": path.join(tmpDir,"/apple-watch/86x86@2x.png"),
-          "apple-watch-29x29@2x": path.join(tmpDir,"/apple-watch/29x29@2x.png"),
-          "apple-watch-24x24@2x": path.join(tmpDir,"/apple-watch/24x24@2x.png"),
-          "apple-watch-98x98@2x": path.join(tmpDir,"/apple-watch/98x98@2x.png"),
-          "apple-watch-29x29@3x": path.join(tmpDir,"/apple-watch/29x29@3x.png"),
-          "apple-watch-27.5x27.5@2x": path.join(tmpDir,"/apple-watch/27.5x27.5@2x.png"),
+          "apple-watch-icon-24@2x.png": path.join(tmpDir,"/apple-watch/icon-24@2x.png"),
+          "apple-watch-icon-27.5@2x.png": path.join(tmpDir,"/apple-watch/icon-27.5@2x.png"),
+          "apple-watch-icon-29@2x.png": path.join(tmpDir,"/apple-watch/icon-29@2x.png"),
+          "apple-watch-icon-29@3x.png": path.join(tmpDir,"/apple-watch/icon-29@3x.png"),
+          "apple-watch-icon-40@2x.png": path.join(tmpDir,"/apple-watch/icon-40@2x.png"),
+          "apple-watch-icon-44@2x.png": path.join(tmpDir,"/apple-watch/icon-44@2x.png"),
+          "apple-watch-icon-86@2x.png": path.join(tmpDir,"/apple-watch/icon-86@2x.png"),
+          "apple-watch-icon-98@2x.png": path.join(tmpDir,"/apple-watch/icon-98@2x.png"),
           "windows-Logo":path.join(tmpDir,"/app/assets/windows/Logo.png"),
           "windows-SmallLogo":path.join(tmpDir,"/app/assets/windows/SmallLogo.png"),
           "windows-Square44x44Logo.scale-100":path.join(tmpDir,"/app/assets/windows/Square44x44Logo.scale-100.png"),
@@ -85,7 +141,7 @@ describe('icons', function() {
 
     });
 
-    after(function() {
+    afterEach(function() {
       fs.deleteDirSync(tmpDir);
     });
 
